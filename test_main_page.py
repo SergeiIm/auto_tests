8b7fbd2a0a1a10cd80a11bbf247b228ab44f6d1a
SergeiIm <128727366+SergeiIm@users.noreply.github.com>
2024-02-14 21:33:52 +0100

    add check login_page after go to after the transition

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from time import sleep
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 # cmd: pytest -v --tb=line --language=en
@@ -7,13 +8,14 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser=browser, url=link)
     page.open()
-    sleep(3)
+    sleep(1)
     page.should_be_login_link()
-    sleep(3)
+    sleep(1)
     page.go_to_login_page()
-    sleep(3)
-
-
+    sleep(1)
+    page_login = LoginPage(browser=browser, url=browser.current_url)
+    sleep(1)
+    page_login.should_be_login_page()
 
 #def test_add_to_cart(browser):
 #    page = ProductPage(url="", browser)   # инициализируем объект Page Object
