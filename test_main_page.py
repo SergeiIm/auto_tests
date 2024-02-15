4727a533827efc56e0dfb32218c8eceff949a0ef
SergeiIm <128727366+SergeiIm@users.noreply.github.com>
2024-02-16 00:06:00 +0100

    delete not use library and old comments.

--- test_main_page.py
+++ test_main_page.py
@@ -16,10 +16,3 @@ def test_guest_can_go_to_login_page(browser):
     page_login = LoginPage(browser=browser, url=browser.current_url)
     sleep(1)
     page_login.should_be_login_page()
-
-#def test_add_to_cart(browser):
-#    page = ProductPage(url="", browser)   # инициализируем объект Page Object
-#    page.open()                           # открываем страницу в браузере
-#    page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
-#    page.add_product_to_cart()            # жмем кнопку добавить в корзину
-#    page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
