c64135448d7ce39af3592f7f31be65af0db1de7c
SergeiIm <128727366+SergeiIm@users.noreply.github.com>
2024-02-16 00:01:47 +0100

    added cladss for new pages: 'product_page' and 'basket_page'

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,17 @@ class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, 'form#register_form')
     SUB_URL = "login"
+
+
+class ProductPageLocators:
+    BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    BASKET_MINI = (By.CSS_SELECTOR, "div.basket-mini")
+    GO_TO_THE_BASKET_BTN = (By.CSS_SELECTOR, "div.basket-mini a")
+    NAME_PRODUCT = (By.CSS_SELECTOR, "div.product_main  h1")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, "div.product_main p.price_color")
+
+
+class BasketPageLocators:
+    BASKET_MINI = (By.CSS_SELECTOR, "div.basket-mini")
+    NAME_PRODUCT = (By.CSS_SELECTOR, "div.product_main h1")
+
