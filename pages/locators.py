16b90854cb91b61925002c4ac7a399433011065d
SergeiIm <128727366+SergeiIm@users.noreply.github.com>
2024-02-18 16:41:34 +0100

    move constant for login from MainPageLocators to the BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,13 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+class MainPageLocators:
+    pass
 
 
 class LoginPageLocators:
@@ -17,9 +22,9 @@ class ProductPageLocators:
     GO_TO_THE_BASKET_BTN = (By.CSS_SELECTOR, "div.basket-mini a")
     NAME_PRODUCT = (By.CSS_SELECTOR, "div.product_main  h1")
     PRICE_PRODUCT = (By.CSS_SELECTOR, "div.product_main p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div#messages > div")
 
 
 class BasketPageLocators:
-    BASKET_MINI = (By.CSS_SELECTOR, "div.basket-mini")
-    NAME_PRODUCT = (By.CSS_SELECTOR, "div.product_main h1")
-
+    BASKET_MINI = (By.CSS_SELECTOR, "div.alert-info div.alertinner strong")
+    NAME_PRODUCT = (By.XPATH, '//*[@id="messages"]/div[1]/div/strong')
