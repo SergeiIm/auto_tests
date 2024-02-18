89159fbdbd1652314df323ddf457f3c6e4d56c12
SergeiIm <128727366+SergeiIm@users.noreply.github.com>
2024-02-18 16:45:51 +0100

    add two methods for check message on page

--- pages/product_page.py
+++ pages/product_page.py
@@ -12,7 +12,17 @@ class ProductPage(BasePage):
         self.is_element_present_save_him_in_dict_objects(*ProductPageLocators.BASKET_MINI, key='basket-mini')
         self.dict_objects['basket-mini'] = self.dict_objects['basket-mini'].text
 
-    def click_add_to_basket_btn_and_pass_the_check_stepik(self):
+    def click_add_to_basket_btn(self):
         self.is_element_present_save_him_in_dict_objects(*ProductPageLocators.BTN_ADD_TO_BASKET, key='add_to_basket')
         self.click_element(key='add_to_basket')
+
+    def pass_the_check_stepik(self):
         self.solve_quiz_and_get_code()
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappear, but should be"
